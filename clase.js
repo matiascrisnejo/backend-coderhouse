e90ad9fe89ddd201ff5453986ab25bd03cc47acc
matycris <crisnejomatias@gmail.com>
2024-01-10 14:07:17 -0300

    primero mejorado

--- clase.js
+++ clase.js
@@ -1,37 +1,56 @@
-class ProductManager {
+class ProductManager{
+    
     constructor(){
-        this.products = []
+        this.products = [];
+    }
+
+    addProduct(product){
+        const {title, description, price, thumnail, code, stock} = product;
+        if(!title || !description || !price || !thumnail ||!code || !stock){
+            console.log("todos los campos son requeridos");
+            return;
+        }
+
+        if(this.products.some((p) => p.code === code)){
+            console.log("el codigo del producto ya existe");
+            return;
+        }
+
+        const id = this.setId();
+        this.products.push({id, ...product})
     }
 
     getProducts(){
         return this.products
     }
 
-    addProduct(title, description, price, thumbnail, code, stock){
-        const newProduct = {
-            id: this.products.length + 1,
-            title,
-            description,
-            price,
-            thumbnail,
-            code,
-            stock
+    getProductsById(id){
+        const product = this.products.find((p) => p.id === id);
+        if(product === undefined){
+            console.log(`el producto con id ${id} no existe`);
         }
-        this.products.push(newProduct)
+        else return product;
     }
 
-    getProductById(pid){
-        const productoId = this.products.findIndex(product => product.id == pid)
-        if (productoId === -1 ) {
-            return `El producto no existe`
-        }
-        return productoId
+    setId(){
+        if(!this.lastId) this.lastId = 1;
+        else this.lastId++;
+        return this.lastId;
     }
-
 
 }
 
 
 const productManager = new ProductManager()
-console.log(productManager.addProduct("coca","es una bebida", 100, "ruta de imagen", 1, 1000))
-console.log(productManager.getProducts())
+const product1 ={
+    title:"title",
+    description: "description",
+    price:"price",
+    thumnail: "thumnail",
+    code:"123",
+    stock: "5",
+};
+
+productManager.addProduct(product1);
+const misProductos = productManager.getProducts()
+console.log(misProductos);
